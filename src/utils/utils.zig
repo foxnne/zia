fa645c44e04e1b72c32175e80cd3b520461ef143
foxnne <foxnne@gmail.com>
2021-03-04 01:38:55 -0600

    added multihashmap bump flecs cleanup

--- src/utils/utils.zig
+++ src/utils/utils.zig
@@ -1,3 +1,4 @@
 pub const FixedList = @import("fixed_list.zig").FixedList;
+pub const MultiHashMap = @import("multihashmap.zig").MultiHashMap;
 pub const Camera = @import("camera.zig").Camera;
 pub const fs = @import("fs.zig");
